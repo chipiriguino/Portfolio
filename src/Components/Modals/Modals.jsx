1b44a0476dfd3192e50c29a010c74ded3b868ecc
Jose Luis <joseluisblascoortiz@hotmail.com>
2021-03-12 11:48:59 +0100

    add cube project

--- src/Components/Modals/Modals.jsx
+++ src/Components/Modals/Modals.jsx
@@ -8,7 +8,7 @@ export default function Modals() {
 
                 <img className="modal-img" src="images/portfolio/love.jpg" alt="love" width="400rem" />
                 <h2>ILOVEHACK</h2> <span className="categories"><i className="fa fa-tag" />App</span>
-                <p>With React,Express,NodeJS,MongoDB</p>
+                <p>With React,Express,NodeJS,MongoDB,CSS</p>
                 <div className="buttons">
                     <a className="button" href="#be-nice">See More</a>
                 </div>
@@ -33,7 +33,7 @@ export default function Modals() {
             <div className="container">
                 <img className="modal-img" src="images/portfolio/palomitas.jpg" alt="palomitas" width="400rem" />
                 <h2>MovieDick</h2><span className="categories"><i className="fa fa-tag" />App</span>
-                <p>With React,Express,NodeJS,MongoDB</p>
+                <p>With React,Express,NodeJS,MongoDB,CSS</p>
                 <div className="buttons">
                     <a className="button" href="#be-nice1">See More</a>
                 </div>
@@ -57,50 +57,33 @@ The app that every movie lover needs.</p>
 
 
             <div className="container">
-                <img className="modal-img" src="images/portfolio/ironman.jpg" height="400px" alt="hero" width="400rem" />
-                <h2>I´m Your Hero</h2><span className="categories"><i className="fa fa-tag" />Web</span>
-                <p>With HTML,CSS,JScript</p>
+                <img className="modal-img" src="images/cuboplaya.jpg" height="400px" alt="hero" width="400rem" />
+                <h2>ElCubo</h2><span className="categories"><i className="fa fa-tag" />E-commerce</span>
+                <p>With React,Express,MongoDB,NodeJS,CSS</p>
                 <div className="buttons">
-                    <a className="button" href="#be-nice2">See More</a>
+                    <a className="button" href="#be-nice3">See More</a>
                 </div>
             </div>
 
 
 
-            <div className="modal" id="be-nice2">
+            <div className="modal" id="be-nice3">
                 <div className="modal-content">
-                    <img className="pop-img"  src="images/portfolio/ironman.webp" alt="ironman" />
-                    <h4>I´M YOUR HERO</h4>
-                    <p><u>I´M YOUR HERO</u> is a website to enjoy marvel heroes and get to know them more thoroughly, with a retro mode for comic lovers.</p>
+                    <img className="pop-img"  src="images/cubo-pip.png" alt="ironman" />
+                    <h4>ElCubo</h4>
+                    <p><u>ElCubo</u> is an ecommerce where you can both buy or sell and leave your comments.
+In addition, 3 different accounts for management (sellers, clients and admin)</p>
                     <div className="align-btn-portfolio">
                     <a href="#close" className="button">Close</a>
-                    <a href="https://chipiriguino.github.io/ProyectoMarvel/" className="button">Go to App</a>
+                    <a href="https://elcubo.herokuapp.com/" className="button">Go to App</a>
                 </div>
                 </div>
                 <a href="#close" className="background">.</a>
             </div>
 
-            {/* <div className="container">
-<img src="images/portfolio/loading.png" alt="loading" />
-<h5>In comming...</h5>
-                        <p>Loading</p>
-    <div className="buttons">
-        <a className="button" href="#be-nice3">See More</a>
-    </div>
-</div>
 
+        
 
-
-<div className="modal" id="be-nice3">
-    <div className="modal-content">
-    <img className="scale-with-grid" src="images/portfolio/loading.png" alt="charging" />
-    <h4>Loading 56%</h4>
-              <p>In comming</p>
-                <a href="#close" className="button">Close</a>
-        <a  href="" className="button">Soon</a>
-    </div>
-    <a href="#close" className="background">.</a>
-</div> */}
 
         </div>
     )
